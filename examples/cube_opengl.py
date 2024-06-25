4bf6272f9f767d6998c3e43a37866ac8089ea292
cyber <lihibaranetz@gmail.com>
2024-06-25 10:59:23 +0300

    with movement

--- examples/cube_opengl.py
+++ examples/cube_opengl.py
@@ -1,103 +1,64 @@
 import pygame
 from pygame.locals import *
+
 from OpenGL.GL import *
 from OpenGL.GLU import *
-import time
-import threading
 
-def Cube(delta):
-    vertices = (
-        (1 + delta, -1 + delta, -1 + delta + 5),
-        (1 + delta, 1 + delta, -1 + delta + 5),
-        (-1 + delta, 1 + delta, -1 + delta + 5),
-        (-1 + delta, -1 + delta, -1 + delta + 5),
-        (1 + delta, -1 + delta, 1 + delta + 5),
-        (1 + delta, 1 + delta, 1 + delta + 5),
-        (-1 + delta, -1 + delta, 1 + delta + 5),
-        (-1 + delta, 1 + delta, 1 + delta + 5)
+verticies = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
     )
 
-    edges = (
-        (0, 1),
-        (0, 3),
-        (0, 4),
-        (2, 1),
-        (2, 3),
-        (2, 7),
-        (6, 3),
-        (6, 4),
-        (6, 7),
-        (5, 1),
-        (5, 4),
-        (5, 7)
+edges = (
+    (0,1),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7)
     )
+
+
+def Cube():
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
-            glVertex3fv(vertices[vertex])
+            glVertex3fv(verticies[vertex])
     glEnd()
 
-def move_cube_right(speed):
-    glTranslatef(speed, 0.0, 0.0)
 
-def set_cube_position_top_left():
-    glTranslatef(-4.0, 3.0, -5)
-
-def set_cube_position_bottom_left():
-    glTranslatef(-8.0, 0.0, -5)
-
-def set_cube_position_right():
-    glTranslatef(-2.0, 0.0, -5)
-
-def move_cube_up(speed):
-    glTranslatef(0.0, speed, 0.0)
-
-def move_cube_in(speed):
-    glTranslatef(0.0, 0.0, speed)
-
-def client_thread(position_function):
+def main():
     pygame.init()
-    display = (800, 600)
-    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
-    gluPerspective(120, (display[0] / display[1]), 0.1, 50.0)
-    gluLookAt(0, 0, 0, 0, 0, -5, 0, 1, 0)
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
-    position_function()
-
-    scale_factor = 0.5
-    last_time = time.time()
+    glTranslatef(0.0,0.0, -5)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                return
+                quit()
 
-        current_time = time.time()
-        if current_time - last_time >= 0.1:
-            move_cube_right(0.1)
-            last_time = current_time
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glPushMatrix()  # Save the current matrix
-        glScalef(scale_factor, scale_factor, scale_factor)  # Scale the cube
-        Cube(0)  # Draw the scaled cube
-        glPopMatrix()  # Restore the previous matrix
-
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        Cube()
         pygame.display.flip()
         pygame.time.wait(10)
 
-def main():
-    threads = []
-    positions = [set_cube_position_top_left, set_cube_position_bottom_left, set_cube_position_right]
-
-    for position in positions:
-        thread = threading.Thread(target=client_thread, args=(position,))
-        thread.start()
-        threads.append(thread)
-
-    for thread in threads:
-        thread.join()
 
 main()
